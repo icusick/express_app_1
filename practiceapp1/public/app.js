d2a8a112945c3df07c7f396d69d6cfd91b19bd8e
isabel cusick <cusick.isabel@gmail.com>
2017-02-21 16:04:16 -0600

    creates angular routes for work and contact pages

--- practiceapp1/public/app.js
+++ practiceapp1/public/app.js
@@ -1,1 +1,23 @@
 var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/main.ejs.html',
+		controller: 'mainController'
+	})
+
+	.when('/second', {
+		templateUrl: 'pages/second.ejs.html',
+		controller: 'secondController'
+	})
+
+	.when('/third', {
+		templateUrl: 'pages/contact.ejs.html',
+		controller: 'thirdController'
+	})
+
+
+});
